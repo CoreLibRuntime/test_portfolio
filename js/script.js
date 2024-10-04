5c672af7b776eea27ef2263ecbf9315370f36911
Haru <JloGuka@yandex.ru>
2024-10-04 22:55:52 +0300

    script addition

---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+let skills = {
+    data : [
+        {skill:'html',level:40,class:'skill-item_html',iconName:'html.svg'},
+        {skill:'css',level:40,class:'skill-item_css',iconName:'css.svg'},
+        {skill:'c#',level:40,class:'skill-item_csharp',iconName:'csharp.svg'},
+        {skill:'cpp',level:40,class:'skill-item_cpp',iconName:'c++.svg'},
+        {skill:'python',level:40,class:'skill-item_python',iconName:'python.svg'},
+    ],
+    generateList(parentElement){
+        this.data.forEach(elem => {
+            let dt = document.createElement('dt')
+            dt.classList.add(elem.class, 'skill-item')
+            dt.textContent=elem.skill
+            dt.style.backgroundImage=`url('../img/skills/${elem.iconName}')`
+            let dd = document.createElement('dd')
+            dd.classList.add('skill-level')
+            let div = document.createElement('div')
+            div.textContent=`${elem.level}%`
+            div.style.width=div.textContent
+            dd.appendChild(div)
+            parentElement.appendChild(dt)
+            parentElement.appendChild(dd)
+        });
+        return parentElement
+    }
+}
+skills.generateList(document.querySelector('dl.skill-list'))
+
+
+
+
+
+
+
+
+
