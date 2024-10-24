2f4f4aef9fce25f9e6cff5135273b760001f4f10
Haru <JloGuka@yandex.ru>
2024-10-25 00:01:32 +0300

    skill sort implement

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 'use strict';
 let skills = {
+    sortMode:null,
     data : [
-        {skill:'html',level:40,class:'skill-item_html',iconName:'html.svg'},
-        {skill:'css',level:40,class:'skill-item_css',iconName:'css.svg'},
-        {skill:'c#',level:40,class:'skill-item_csharp',iconName:'csharp.svg'},
-        {skill:'cpp',level:40,class:'skill-item_cpp',iconName:'c++.svg'},
-        {skill:'python',level:40,class:'skill-item_python',iconName:'python.svg'},
+        {name:'html',level:40,class:'skill-item_html',iconName:'html.svg'},
+        {name:'css',level:40,class:'skill-item_css',iconName:'css.svg'},
+        {name:'c#',level:40,class:'skill-item_csharp',iconName:'csharp.svg'},
+        {name:'cpp',level:40,class:'skill-item_cpp',iconName:'c++.svg'},
+        {name:'python',level:40,class:'skill-item_python',iconName:'python.svg'},
     ],
+    getComparator(prop){
+        return (a,b)=> a[prop]>b[prop] ? 1 : a[prop]<b[prop] ? -1:0
+    },
+    sortList(type){
+        if(this.sortMode!=type) {
+            this.data.sort(this.getComparator(type));
+            console.log(`ОТСОРТИРОВАЛИ ПО ${type}`)
+        }
+        else{
+            this.data.reverse();
+            console.log(`ИНВЕРТИРОВАЛИ ПО ${type}`)
+        } 
+        this.sortMode=type;
+        this.generateList(document.querySelector('dl.skill-list'));
+    },
     generateList(parentElement){
+        parentElement.innerHTML=""
         this.data.forEach(elem => {
             let dt = document.createElement('dt')
             dt.classList.add(elem.class, 'skill-item')
-            dt.textContent=elem.skill
+            dt.textContent=elem.name
             dt.style.backgroundImage=`url('./img/skill_${elem.iconName}')`
             let dd = document.createElement('dd')
             dd.classList.add('skill-level')
@@ -26,6 +43,19 @@ let skills = {
     }
 }
 skills.generateList(document.querySelector('dl.skill-list'))
+document.getElementsByClassName("skills-sort")[0].addEventListener("click",function(e){
+    if(e.target.tagName==="BUTTON"){
+        switch(e.target.dataset.type){
+            case 'name':
+            case 'level':
+                skills.sortList(e.target.dataset.type)
+                break;
+            default:
+                console.log(e.target.tagName)
+                break;
+        }
+    }
+});
 
 
 
